fba20f36e6ba1c8503596e58a38b0e229a3f37aa
zzaakiirr <zzaakiirr@gmail.com>
2018-10-30 22:06:44 +0300

    Added polls app + Migrated it with cms

--- ns-consulting/polls/models.py
+++ ns-consulting/polls/models.py
@@ -1,3 +1,32 @@
+from django.utils.translation import ugettext_lazy as _
+
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(
+        verbose_name=_('Вопрос'),
+        max_length=255,
+        blank=False,
+        default=None,
+    )
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Выбор'
+        verbose_name_plural = 'Выборы'
+
+    def __str__(self):
+        return self.choice_text
--- ns-consulting/polls/views.py
+++ ns-consulting/polls/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
-# Create your views here.
+from .models import Choice, Poll
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        return Poll.objects.all()[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+
+def vote(request, poll_id, choice_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    selected_choice = poll.choice_set.get(pk=choice_id)
+
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
