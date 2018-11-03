448e4bd7fc85731e7acaff3b2ce56dffb7aa76c1
zzaakiirr <zzaakiirr@gmail.com>
2018-11-03 18:42:46 +0300

    Added basic search view + posts/search_results template

--- ns-consulting/posts/views.py
+++ ns-consulting/posts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import operator
+from functools import reduce
 
-# Create your views here.
+from django.db.models import Q
+from django.views.generic import ListView
+
+from .models import PostPluginModel
+
+
+class PostSearchListView(ListView):
+    model = PostPluginModel
+    paginate_by = 10
+    template_name = 'posts/search_results.html'
+
+    def get_queryset(self):
+        search_result = super(PostSearchListView, self).get_queryset()
+
+        query = self.request.GET.get('q')
+        if query:
+            query_list = query.split()
+            search_result = search_result.filter(
+                reduce(operator.and_,
+                       (Q(title__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                       (Q(body__icontains=q) for q in query_list))
+            )
+
+        return search_result
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearchListView, self).get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        context['search_query'] = query
+        context['posts'] = self.get_queryset()
+        return context
