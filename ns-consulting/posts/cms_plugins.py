ca9ad3ba025d767f1c844a60ad43f5e28fefa20a
zzaakiirr <zzaakiirr@gmail.com>
2018-11-03 13:38:25 +0300

    Refactored PostPluginModel to inherit from AbstractText + changed its Post PluginPublisher + Beautified posts/admin.py + Added migrations

--- ns-consulting/posts/cms_plugins.py
+++ ns-consulting/posts/cms_plugins.py
@@ -1,18 +1,20 @@
 from django.utils.translation import ugettext as _
 
-from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from djangocms_text_ckeditor.cms_plugins import TextPlugin
 
 from .models import PostPluginModel
 
 
 @plugin_pool.register_plugin
-class PostPluginPublisher(CMSPluginBase):
+class PostPluginPublisher(TextPlugin):
     model = PostPluginModel
     module = _("Новости")
     name = _("Новость")
     render_template = "posts/post_plugin.html"
 
     def render(self, context, instance, placeholder):
-        context.update({'instance': instance})
-        return context
+        context.update({'name': instance.name, 'instance': instance, })
+        return super(PostPluginModel, self).render(
+            context, instance, placeholder
+        )
--- ns-consulting/posts/models.py
+++ ns-consulting/posts/models.py
@@ -3,21 +3,15 @@ from django.utils.translation import ugettext_lazy as _
 
 from cms.models import CMSPlugin
 from cms.models.fields import PageField
-from djangocms_text_ckeditor.fields import HTMLField
+from djangocms_text_ckeditor.models import AbstractText
 
 
-class Post(models.Model):
+class PostPluginModel(AbstractText):
     title = models.CharField(
         verbose_name=_('Заголовок'),
         max_length=255,
         blank=True,
     )
-
-    description = HTMLField(
-        verbose_name=_('Описание'),
-        blank=True,
-    )
-
     link = PageField(
         verbose_name=_('Ссылка'),
         blank=False,
@@ -28,20 +22,7 @@ class Post(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
 
-    def __str__(self):
+    def __unicode__(self):
         if self.title:
             return self.title
         return 'новость %d' % self.id
-
-
-class PostPluginModel(CMSPlugin):
-    post = models.ForeignKey(Post)
-
-    def __unicode__(self):
-        if self.post.title:
-            return self.post.title
-        return 'новость %d' % self.post.id
-
-    class Meta:
-        verbose_name = 'Новость'
-        verbose_name_plural = 'Новости'
