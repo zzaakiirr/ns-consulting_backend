4a8b414a1439056d36ac61f829375841a38f86c2
zzaakiirr <zzaakiirr@gmail.com>
2018-11-04 13:32:53 +0300

    Beautified posts/views.py code

--- ns-consulting/posts/views.py
+++ ns-consulting/posts/views.py
@@ -3,6 +3,7 @@ from functools import reduce
 
 from django.db.models import Q
 from django.views.generic import ListView
+from django.urls.exceptions import NoReverseMatch
 
 from .models import PostPluginModel
 
@@ -12,25 +13,27 @@ class PostSearchListView(ListView):
     template_name = 'posts/search_results.html'
 
     def get_queryset(self):
-        search_result = super(PostSearchListView, self).get_queryset()
-        query = self.request.GET.get('q')
+        search_results = super(PostSearchListView, self).get_queryset()
 
+        query = self.request.GET.get('q')
         if query:
             query_list = query.split()
-            search_result = search_result.filter(
+            search_results = search_results.filter(
                 reduce(
                     operator.and_,
                     (Q(body__icontains=q) for q in query_list)
                 )
             )
 
-        return search_result
+        return search_results
 
     def get_context_data(self, **kwargs):
         context = super(PostSearchListView, self).get_context_data(**kwargs)
-        query = self.request.GET.get('q')
 
+        query = self.request.GET.get('q')
         context['search_query'] = query
-        context['posts'] = self.get_queryset()
+
+        posts = self.get_queryset()
+        context['posts'] = posts
 
         return context
