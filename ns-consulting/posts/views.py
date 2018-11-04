6a00b4fa32fe8c794fa4d7f4df4cfbb6c12123fb
zzaakiirr <zzaakiirr@gmail.com>
2018-11-04 15:02:27 +0300

    Posts search: Fixed returning duplicate posts + Fixed None query request printing 'None' to template + Beautified search_results template

--- ns-consulting/posts/views.py
+++ ns-consulting/posts/views.py
@@ -7,6 +7,8 @@ from django.urls.exceptions import NoReverseMatch
 
 from .models import PostPluginModel
 
+from .search_helpers import get_duplicate_ids
+
 
 class PostSearchListView(ListView):
     model = PostPluginModel
@@ -16,14 +18,16 @@ class PostSearchListView(ListView):
         search_results = super(PostSearchListView, self).get_queryset()
 
         query = self.request.GET.get('q')
-        if query:
-            query_list = query.split()
-            search_results = search_results.filter(
-                reduce(
-                    operator.and_,
-                    (Q(body__icontains=q) for q in query_list)
-                )
+        if not query:
+            return None
+
+        query_list = query.split()
+        search_results = search_results.filter(
+            reduce(
+                operator.and_,
+                (Q(body__icontains=q) for q in query_list)
             )
+        ).exclude(id__in=get_duplicate_ids())
 
         return search_results
 
@@ -31,9 +35,12 @@ class PostSearchListView(ListView):
         context = super(PostSearchListView, self).get_context_data(**kwargs)
 
         query = self.request.GET.get('q')
-        context['search_query'] = query
+        if not query:
+            context['search_query'] = ''
+        else:
+            context['search_query'] = query
 
-        posts = self.get_queryset()
-        context['posts'] = posts
+        search_results = self.get_queryset()
+        context['search_results'] = search_results
 
         return context
