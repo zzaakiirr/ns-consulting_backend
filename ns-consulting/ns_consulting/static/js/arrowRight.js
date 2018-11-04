a52bf8d128c218d74f8871bbdc54a8f413cd15a5
zzaakiirr <zzaakiirr@gmail.com>
2018-11-04 20:06:56 +0300

    Replaced banner-right-1-XS block upper + Added Right arrow for this block + js file which add href to arrow img

---
+++ ns-consulting/ns_consulting/static/js/arrowRight.js
@@ -0,0 +1,3 @@
+var bannersRight1XS = document.getElementsByClassName('banner-right-1-XS');
+var bannerRight1XS = bannersRight1XS[0];
+document.getElementById('arrowRight').href = bannerRight1XS.childNodes[2].href;
